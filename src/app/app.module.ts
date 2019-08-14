5e3308caa21e6d9c43ef493cbc7a094476eb3796
fmsotelo <fmsotelo75@gmail.com>
2019-08-13 22:59:20 -0700

    Beginning to style the nav bar or header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
